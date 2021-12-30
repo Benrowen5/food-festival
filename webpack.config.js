e9101c44304c254079c75721ff6df2e80fa07223
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-12-30 10:00:01 -0500

    added service worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,53 @@
-const path = require('path');
 const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const path = require('path');
 
-module.exports = {
+const config = {
+    mode: 'development',
     entry: {
-        app: './assets/js/script.js',
-        events: './assets/js/events.js',
-        schedule: './assets/js/schedule.js',
-        tickets: './assets/js/tickets.js'
+      app: './assets/js/script.js',
+      events: './assets/js/events.js',
+      schedule: './assets/js/schedule.js',
+      tickets: './assets/js/tickets.js'
     },
     output: {
-        path: path.join(__dirname, '/dist'),
-        filename: '[name].bundle.js',
+      filename: '[name].bundle.js',
+      path: path.join(__dirname + '/dist')
     },
-    module: {
-        rules: [
+    module: { 
+      rules: [
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
             {
-                test: /\.jpg$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            esModule: false,
-                            name (file) {
-                                return '[path][name].[ext]'
-                            },
-                            publicPath: function(url) {
-                                return url.replace('../', '/assets')
-                            }
-                        }                        
-                    },
-                    {
-                        loader: 'image-webpack-loader'
-                    }
-                ]
+              loader: 'file-loader',
+              options: {
+                esModule: false,
+                name(file) {
+                  return '[path][name].[ext]';
+                },
+                publicPath(url) {
+                  return url.replace('../', '/assets/');
+                }
+              }
+            },
+            {
+              loader: 'image-webpack-loader'
             }
-        ]
+          ]
+        }
+      ]
     },
     plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        }),
-        new BundleAnalyzerPlugin({
-            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-        })
-    ],
-    mode: 'development'
-};
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery'
+      }),
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'static'
+      })
+    ]
+  };
+  
+  module.exports = config;
+  
